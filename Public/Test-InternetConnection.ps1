4f84104005c3a6e72da18c479ccf25aaea2cca61
Anthony Cotales <ton.cotales@gmail.com>
2026-03-17 16:13:49 +0800

    Improve Test-InternetConnection function
    Added enhanced internet connection testing with ICMP and HTTP fallback.

--- Public/Test-InternetConnection.ps1
+++ Public/Test-InternetConnection.ps1
@@ -2,12 +2,35 @@ function Test-InternetConnection {
     [CmdletBinding()]
     param()
 
-    try {
-        return Test-NetConnection -InformationLevel Quiet
-    }
-    catch {
-        Write-Error "An error occured while testing the connection: $_"
-        return $false
+    $pingTestParams = @{
+        ComputerName = "8.8.8.8"
+        Count        = 1
+        Quiet        = $true
+        ErrorAction  = "SilentlyContinue"
     }
 
+    $httpTestParams = @{
+        Uri             = "https://www.google.com/generate_204"
+        UseBasicParsing = $true
+        TimeoutSec      = 5
+        ErrorAction     = "Stop"
+    }
+
+    try {
+        # Fast ICMP check
+        if (Test-Connection @pingTestParams) {
+            return $true 
+        }
+
+        # HTTP fallback
+        $response = Invoke-WebRequest @httpTestParams
+        if ($response.StatusCode -eq 204) {
+            return $true 
+        }   
+    }
+    catch {
+        Write-Error "Test-InternetConnection Error: $_"
+    } 
+    
+    return $false
 }
